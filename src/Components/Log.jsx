4729dd0e09881f993471725b05e9f2819a8defd2
AntonyShinson <160327504+AntonyShinson@users.noreply.github.com>
2025-12-07 16:11:02 +0530

    Clean up Log.jsx by removing unnecessary comment
    Removed irrelevant comment and added personal information.

--- src/Components/Log.jsx
+++ src/Components/Log.jsx
@@ -4,4 +4,4 @@ export default function Log({turns})
     {turns.map((turn=><li key={`${turn.square.row}${turn.square.col}`}>{turn.player} selected {turn.square.row} ,{turn.square.col}</li>))}
     </ol>
 }
-#intershpi and related field
+#hello i am anntony shinson studying in scms college if enginjeering and technology and i am studying in coputer related fie;d an how withnes can occuur in the cancel in the field and also
