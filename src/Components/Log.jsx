0e11dc492fcdb091bec824233c89c317e55f42ae
AntonyShinson <160327504+AntonyShinson@users.noreply.github.com>
2025-12-07 16:14:57 +0530

    Remove typo comment from Log component

--- src/Components/Log.jsx
+++ src/Components/Log.jsx
@@ -4,4 +4,3 @@ export default function Log({turns})
     {turns.map((turn=><li key={`${turn.square.row}${turn.square.col}`}>{turn.player} selected {turn.square.row} ,{turn.square.col}</li>))}
     </ol>
 }
-#typing fater in com
