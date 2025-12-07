bc41fa94d1dffe3e475aaa1f3b6ff65839403e60
AntonyShinson <160327504+AntonyShinson@users.noreply.github.com>
2025-12-07 16:12:49 +0530

    Remove commented-out code from Log component

--- src/Components/Log.jsx
+++ src/Components/Log.jsx
@@ -4,4 +4,3 @@ export default function Log({turns})
     {turns.map((turn=><li key={`${turn.square.row}${turn.square.col}`}>{turn.player} selected {turn.square.row} ,{turn.square.col}</li>))}
     </ol>
 }
-#hello in tngf jjyf jyfjyf kloi gjf ytdytf fyty td
