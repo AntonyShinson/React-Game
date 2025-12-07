bd824b646b2bc1e9505a4ad5d38ee443364b3f05
AntonyShinson <160327504+AntonyShinson@users.noreply.github.com>
2025-12-07 16:09:44 +0530

    Fix JSX syntax in Log component

--- src/Components/Log.jsx
+++ src/Components/Log.jsx
@@ -4,3 +4,4 @@ export default function Log({turns})
     {turns.map((turn=><li key={`${turn.square.row}${turn.square.col}`}>{turn.player} selected {turn.square.row} ,{turn.square.col}</li>))}
     </ol>
 }
+#intershpi and related field
